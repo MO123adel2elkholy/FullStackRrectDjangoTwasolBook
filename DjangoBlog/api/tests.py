2a853a7e8586fed52d437ea9d5facd2d0209533a
mahmoud adel elkholy <adel333mahmoud@gmail.com>
2026-08-26 03:20:26 +0200

    Test :uint testing for  test Models dennder methods for post and category models through django TestCase  and test endpoint create and display posts endpoint through using  restframework APITestCase

--- DjangoBlog/api/tests.py
+++ DjangoBlog/api/tests.py
@@ -1,3 +1,34 @@
-from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
+from blog.models import Post, Category
+from django.contrib.auth.models import User
+from rest_framework.test import APIClient
 
-# Create your tests here.
+
+class PostTests(APITestCase):
+
+    def test_view_posts(self):
+        """
+        Ensure we can view all objects.
+        """
+        url = reverse('api:listcreate')
+        response = self.client.get(url, format='json')
+        self.assertEqual(response.status_code,status.HTTP_200_OK)
+    
+    def test_create_post(self):
+        """
+        Ensure we can create a new Post object and view object.
+        """
+        self.test_category = Category.objects.create(name='django')
+        self.testuser1 = User.objects.create_superuser(
+            username='test_user1', password='123456789' , email='adel333mahmoud@gmail.com')
+
+        self.client.login(username=self.testuser1.username,
+                          password='123456789')
+
+        data = {"title": "new", "author": 1,
+                "excerpt": "new", "content": "new" , 'category':1}
+        url = reverse('api:listcreate')
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
