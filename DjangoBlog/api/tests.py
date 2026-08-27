3fb5c135a2f662c2e602241c8c83547e89f53861
mahmoud adel elkholy <adel333mahmoud@gmail.com>
2026-08-28 01:06:06 +0200

    Testing: Enhance post creation and update tests with user authentication and detailed data validation

--- DjangoBlog/api/tests.py
+++ DjangoBlog/api/tests.py
@@ -3,7 +3,6 @@ from rest_framework import status
 from rest_framework.test import APITestCase
 from blog.models import Post, Category
 from django.contrib.auth.models import User
-from rest_framework.test import APIClient
 
 
 class PostTests(APITestCase):
@@ -14,21 +13,70 @@ class PostTests(APITestCase):
         """
         url = reverse('api:listcreate')
         response = self.client.get(url, format='json')
-        self.assertEqual(response.status_code,status.HTTP_200_OK)
-    
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
     def test_create_post(self):
         """
         Ensure we can create a new Post object and view object.
         """
         self.test_category = Category.objects.create(name='django')
         self.testuser1 = User.objects.create_superuser(
-            username='test_user1', password='123456789' , email='adel333mahmoud@gmail.com')
+            username='test_user1',
+            password='123456789',
+            email='adel333mahmoud@gmail.com',
+        )
 
-        self.client.login(username=self.testuser1.username,
-                          password='123456789')
+        self.client.login(username=self.testuser1.username, password='123456789')
 
-        data = {"title": "new", "author": 1,
-                "excerpt": "new", "content": "new" , 'category':1}
+        data = {
+            "title": "new",
+            "author": self.testuser1.id,
+            "excerpt": "new",
+            "content": "new",
+            "category": self.test_category.id,
+            "slug": "new",
+            "status": "published",
+        }
+
         url = reverse('api:listcreate')
         response = self.client.post(url, data, format='json')
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def test_post_update(self):
+        self.test_category = Category.objects.create(name='django')
+        self.testuser1 = User.objects.create_user(
+            username='test_user1', password='123456789'
+        )
+        self.testuser2 = User.objects.create_user(
+            username='test_user2', password='123456789'
+        )
+
+        test_post = Post.objects.create(
+            category=self.test_category,
+            title='Post Title',
+            excerpt='Post Excerpt',
+            content='Post Content',
+            slug='post-title',
+            author=self.testuser1,
+            status='published',
+        )
+
+        self.client.login(username=self.testuser1.username, password='123456789')
+
+        url = reverse('api:detaildestroy', kwargs={'pk': test_post.pk})
+
+        response = self.client.put(
+            url,
+            {
+                "title": "New",
+                "author": self.testuser1.id,
+                "excerpt": "New",
+                "content": "New",
+                "slug": "new-post-title",
+                "status": "published",
+                "category": self.test_category.id,
+            },
+            format='json',
+        )
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
