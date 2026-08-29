2ee73b238c86ea4c927e659979a9b9490459f171
mahmoud adel elkholy <adel333mahmoud@gmail.com>
2026-08-30 01:52:53 +0200

    Update : Update bckend Stack from Django 3.1 to django 4.2 stable version and supporting social authentication and relative packages

--- DjangoBlog/api/views.py
+++ DjangoBlog/api/views.py
@@ -3,8 +3,7 @@ from rest_framework.generics import ListCreateAPIView , RetrieveUpdateDestroyAPI
 from .serializers import PostSerializer
 from blog.models import  Post , Category
 from rest_framework.permissions import IsAuthenticated , SAFE_METHODS , BasePermission , IsAuthenticatedOrReadOnly
-
-
+# from django_filters.conf import settings
 class PostChangPermission(BasePermission):
     message="Post Editing or deleting is only alowed for authenticated user and user owens the post "
     def has_object_permission(self, request, view, obj):
